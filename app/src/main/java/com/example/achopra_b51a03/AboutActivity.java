106213135bd27e9d7734f3ca48cf7f12eed6ea6b
AdiChops <Chopra.Aaditya@cegep-heritage.qc.ca>
2019-11-07 23:47:57 -0500

    about and main activity completed

---
+++ app/src/main/java/com/example/achopra_b51a03/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.example.achopra_b51a03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
---
+++ app/src/main/java/com/example/achopra_b51a03/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.achopra_b51a03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnAbout = findViewById(R.id.btnAbout);
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), AboutActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
